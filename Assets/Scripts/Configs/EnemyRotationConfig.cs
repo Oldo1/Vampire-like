aeafa3a1c466992d69e1f562b61bd380413d7244
OldVergil <semenichenko13@gmail.com>
2026-09-21 11:25:46 +0800

    Добавление классов конфигурации для вращения врагов и снарядов

---
+++ Assets/Scripts/Configs/EnemyRotationConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Configs
+{
+    [CreateAssetMenu(fileName = "EnemyRotationConfig", menuName = "Configs/Enemy Rotation")]
+    public class EnemyRotationConfig : ScriptableObject
+    {
+        [field: SerializeField, Min(0)] public float RotationSpeed { get; private set; } = 720f;
+    }
+}
---
+++ Assets/Scripts/Configs/ProjectileConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Configs
+{
+    [CreateAssetMenu(fileName = "ProjectileConfig", menuName = "Configs/Projectile")]
+    public class ProjectileConfig : ScriptableObject
+    {
+        [field: SerializeField, Min(0)] public float ProjectileLifetime { get; private set; } = 5f;
+    }
+}
